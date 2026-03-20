3793783385ba8d8dfadeb846211bbfb87eb1e2e4
nolan freeman abbott <nolan@freemanabbott.com>
2026-03-20 11:39:56 -0500

    refactor tests

--- test/test_vector.ml
+++ test/test_vector.ml
@@ -3,38 +3,34 @@ open Ocamllib
 
 module VectorTest = struct
   module Make = struct
-    let unary_oper_test ~oper name a result =
-      name >:: (fun _ -> assert_equal ~cmp:Vector.same result (oper a))
+    let unary_oper_test ~oper name vec result =
+      name >:: (fun _ -> assert_equal ~cmp:Vector.same result (oper vec))
 
-    let unary_fold_test ~oper name a result =
-      name >:: (fun _ -> assert_equal ~cmp:Float.equal result (oper a))
+    let unary_fold_test ~oper name vec result =
+      name >:: (fun _ -> assert_equal ~cmp:Float.equal result (oper vec))
 
     let binary_oper_test ~oper name a b result =
       name >:: (fun _ -> assert_equal ~cmp:Vector.same result (oper a b))
 
     let binary_fold_test ~oper name a b result =
       name >:: (fun _ -> assert_equal ~cmp:Float.equal result (oper a b))
+
+    let validate_length_test name vec len =
+      name >:: (fun _ -> assert_equal (Vector.dim vec) len)
+
+    let expect_fail_test name expr =
+        name >:: (fun _ -> assert_bool "" (try ignore (expr ()); false with | _ -> true))
   end
-
-  let invalid_length =
-    (fun _ ->
-       assert_raises
-         (Invalid_argument "Vector.make: negative dimension")
-         (fun () -> Vector.make (-1) 0.0)
-    )
-
-  let valid_length =
-    (fun _ ->
-       let n = 5 in
-       let v = Vector.make n 0.0 in
-       assert_equal n (Vector.dim v)
-    )
 end
 
 let test_vec = "vector test suite" >::: [
     (* structure *)
-    "invalid_length" >:: VectorTest.invalid_length;
-    "valid_length" >:: VectorTest.valid_length;
+    VectorTest.Make.validate_length_test "valid_length"
+      (Vector.make 5 0.0)
+      5;
+
+    VectorTest.Make.expect_fail_test "invalid_length"
+      (fun _ -> Vector.make (-1) 0.0);
 
     (* conversions *)
 
