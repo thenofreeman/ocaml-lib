5ebdd1a030937652cc5c1ac2416987259c69618b
nolan freeman abbott <nolan@freemanabbott.com>
2026-03-20 04:32:04 -0500

    broken

--- data_frame.ml
+++ data_frame.ml
@@ -1,10 +1,16 @@
-module Data = struct
-  let to_matrix l =
+module DataFrame = struct
+  type 'a t = {
+    data : 'a array;
+    rows : int;
+    cols : int
+  }
+
+  let to_matrix df =
     let nrows = List.length l in
     let ncols = List.length (List.hd l) in
     let mat = make nrows ncols 0.0 in
     List.iteri (fun i row ->
-        if List.length row <> ncols then invalid_arg "of_data_matrix: data_matrix is jagged";
+        if List.length row <> ncols then invalid_arg "of_matrix: data_frame is jagged";
         let row = Array.of_list (List.map (fun x -> Float.of_string x) row) in
         Array.blit mat.data nrows row nrows ncols
       ) l;
@@ -12,8 +18,4 @@ module Data = struct
 
   (* TODO *)
   (* let of_matrix mat = () *)
-
-  let from_csv_float ?(hasheader=false) filename =
-    (* TODO: skip headers if hasheader is true *)
-    to_matrix (Csv.load filename)
 end
